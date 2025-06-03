ef5581be1c5f39ae1546f8a8b2fa1a5c00133d37
EldritchParishioner <deemasfajr@gmail.com>
2025-06-03 23:13:44 +0700

    lagiiiiii

--- main.tf
+++ main.tf
@@ -20,6 +20,11 @@ variable "create_bucket" {
   default     = true
 }
 
+# Membuat timestamp unik untuk nama bucket
+locals {
+  unique_suffix = substr(md5(timestamp()), 0, 6)
+}
+
 provider "google" {
   credentials = file(var.credentials)
   project     = var.project
@@ -28,6 +33,10 @@ provider "google" {
 
 resource "google_storage_bucket" "static" {
   count    = var.create_bucket ? 1 : 0
-  name     = "${var.project}-static-bucket"
+  name     = "${var.project}-static-bucket-${local.unique_suffix}"
   location = var.region
+
+  # Opsional: set storage class dan akses level sesuai kebutuhan
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
 }
