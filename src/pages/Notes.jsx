e6ac98ac6c12d86730a276d2470ebb331f3cb2c9
Alfa Renaldo Aluska <renaldoaluska@gmail.com>
2025-06-04 19:44:05 +0700

    Change Our Notes jadi MyNotes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -31,7 +31,7 @@ const Notes = ({ notes }) => {
       >
         {!showSearch && (
           <h2 className="text-[28px] text-white ml-5 font-['Poppu']">
-            Our Notes
+            MyNotes
           </h2>
         )}
         {showSearch && (
